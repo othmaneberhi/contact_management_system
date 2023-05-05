fd1b694667730cf08eaa4bce26527c8435c0607a
OTHMANE BERHI <omaxnoti@gmail.com>
2023-05-05 07:56:01 +0000

    init, first commit

---
+++ src/main/java/com/ensah/gs_contact/GsContactApplication.java
@@ -0,0 +1,13 @@
+package com.ensah.gs_contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensah/gs_contact/GsContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensah.gs_contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
