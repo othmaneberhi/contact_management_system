15bea2c994533d88fcea878a204f6e188c9e32c2
Othmane berhi <omaxnoti@gmail.com>
2023-05-10 04:42:43 +0000

    Implementing delete methode for contact

--- src/main/java/com/ensah/gs_contact/controller/ContactController.java
+++ src/main/java/com/ensah/gs_contact/controller/ContactController.java
@@ -1,15 +1,15 @@
 package com.ensah.gs_contact.controller;
 
 import com.ensah.gs_contact.bo.contact.Contact;
+import com.ensah.gs_contact.bo.message.Message;
+import com.ensah.gs_contact.bo.message.MessageType;
 import com.ensah.gs_contact.service.contact.IContactService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ContactController {
@@ -28,9 +28,9 @@ public class ContactController {
     }
 
     @PostMapping("/contacts/add")
-    public String addContact(@ModelAttribute("contact") Contact contact) {
-        System.out.println(contact);
+    public String addContact(@ModelAttribute("contact") Contact contact,Model model) {
         contactService.addContact(contact);
+        model.addAttribute("message",new Message("Contact added successfully ", MessageType.SUCCESS));
         return "redirect:/contacts";
     }
 
@@ -39,5 +39,16 @@ public class ContactController {
         List<Contact> contactList = (List<Contact>) contactService.getAllContactsByOrderByLastName();
         model.addAttribute("contacts",contactList);
         return "contacts";
+    }
+
+    @PostMapping("/contacts/delete/{id}")
+    public String deleteContact(@PathVariable("id") Long id,Model model){
+        Optional<Contact> contactToDelete = contactService.getContactById(id);
+        if(!contactToDelete.isPresent()){
+           model.addAttribute("message",new Message("Contact not foung", MessageType.ERROR));
+        }
+        contactService.deleteContact(contactToDelete.get());
+        model.addAttribute("message",new Message("Contact deleted successfully ", MessageType.SUCCESS));
+        return "redirect:/contacts";
     }
 }
