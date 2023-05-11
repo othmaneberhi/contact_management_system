e9cfdc242e72080a080f7f9f2a8c1894c9b649f2
Othmane berhi <omaxnoti@gmail.com>
2023-05-11 02:08:41 +0000

    Implementing exceptions (NotFoundException)

---
+++ src/main/java/com/ensah/gs_contact/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.ensah.gs_contact.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
